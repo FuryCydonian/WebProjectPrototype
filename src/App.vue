cc72bedf6a3f5b7c7de080542552069665eb0654
Fury <ella.bir@hotmail.com>
2019-11-03 03:23:01 +0300

    change app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,42 @@
 <template>
   <v-app>
-    
+      <v-navigation-drawer 
+        app 
+        dark
+        v-model="drawer"
+      >
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              Show Geek
+            </v-list-item-title>
+            <!-- <v-list-item-subtitle>
+              subtext
+            </v-list-item-subtitle> -->
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list
+          dense
+          nav
+        >
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
   </v-app>
 </template>
 
@@ -11,7 +47,16 @@ export default {
   components: {
   },
   data: () => ({
-    //
+    drawer: null,
+    items: [
+      { title: 'Профиль', icon: 'mdi-account-outline' },
+      { title: 'Календарь', icon: 'mdi-calendar-month' },
+      { title: 'Диалоги', icon: 'mdi-forum-outline' },
+      { title: 'Закладки', icon: 'mdi-bookmark-multiple-outline' },
+      { title: 'Оповещения', icon: 'mdi-bell-ring-outline' },
+      { title: 'Настройки', icon: 'mdi-settings-outline' },
+    ],
+    right: null,
   }),
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 
@@ -16,8 +17,48 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: About// () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: About// () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: About// () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: About// () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: About// () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: About// () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
 ]
 
 const router = new VueRouter({
