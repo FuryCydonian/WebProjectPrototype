3e0fafa463c4971e7c44a12722b2ef5e78b48fa3
Fury <ella.bir@hotmail.com>
2019-11-10 04:14:55 +0300

    mod. Login @ Registration

---
+++ src/views/Auth/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12>
+        <h1>Login</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'login',
+  components: {
+  },
+  data: () => ({
+    
+  })
+}
+</script>
---
+++ src/views/Auth/Registration.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12>
+        <h1>Registration</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'registration',
+  components: {
+  },
+  data: () => ({
+    
+  })
+}
+</script>
