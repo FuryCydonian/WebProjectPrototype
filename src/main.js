05422d8b5d23274ccd537cea74556a50a8f95aa3
Fury <ella.bir@hotmail.com>
2019-11-15 09:36:19 +0300

    +firebase

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import * as fb from 'firebase'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 
@@ -12,5 +13,17 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created: () => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyBANqkI6e0mwprbOG3031bzbk_BrMMBZQU",
+      authDomain: "showgeek-bc0a1.firebaseapp.com",
+      databaseURL: "https://showgeek-bc0a1.firebaseio.com",
+      projectId: "showgeek-bc0a1",
+      storageBucket: "showgeek-bc0a1.appspot.com",
+      messagingSenderId: "464641109965",
+      appId: "1:464641109965:web:092d8f5ad8e97a513c9e84",
+      measurementId: "G-ZD42GZYH5D"
+    };
+  }
 }).$mount('#app')
